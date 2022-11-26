b10d600232e6cfd303911c1da445900258fc13fa
erica-as <erica.asnt@hotmail.com>
2022-11-26 17:16:13 -0300

    aplicação que reproduz date e hora no console

---
+++ VariaveisHourDate.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoraData
+{
+    public class VariaveisHourDate
+    {
+        public string mes;
+        public string diaSemana;
+        public string version = "1.0";
+    }
+}
