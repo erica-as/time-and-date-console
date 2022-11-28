91bd0fa7f2d11fd478dc0ec237f2b44655333357
erica-as <erica.asnt@hotmail.com>
2022-11-27 22:11:28 -0300

    ajustes aplicação

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 
 namespace HoraData
 {
@@ -9,90 +8,8 @@ namespace HoraData
         {
             VariaveisHourDate hourDate = new VariaveisHourDate();
 
-            Console.WriteLine($"Hora e Data - Versão {hourDate.version}");
-            Console.WriteLine("-------------");
-            Console.WriteLine("Para parar, pressione CTRL + C para sair do programa!\n");
-
-            while(true) 
-            {
-                switch ((int)DateTime.Now.DayOfWeek)
-                {
-                    case 0:
-                       hourDate.diaSemana = "Domingo";
-                       break;
-                    case 1: 
-                       hourDate.diaSemana = "Segunda-Feira";
-                       break;
-                    case  2:
-                       hourDate.diaSemana = "Terça-Feira";
-                       break;
-                    case 3: 
-                       hourDate.diaSemana = "Quarta-Feira";
-                       break;
-                    case 4:
-                       hourDate.diaSemana = "Quinta-Feira";
-                       break;
-                    case 5: 
-                       hourDate.diaSemana = "Sexta-Feira";
-                       break;
-                    case 6: 
-                       hourDate.diaSemana = "Sábado";
-                       break;
-                default: 
-                    hourDate.diaSemana = "Não se Aplica";
-                    break;
-                }
-
-                switch((int)DateTime.Now.Month)
-                {
-                    case 1: 
-                       hourDate.mes = "Janeiro";
-                       break;
-                    case 2: 
-                       hourDate.mes = "Fevereiro";
-                       break;
-                    case 3: 
-                       hourDate.mes = "Março";
-                       break;
-                    case 4: 
-                       hourDate.mes = "Abril";
-                       break;
-                    case 5: 
-                       hourDate.mes = "Maio";
-                       break;
-                    case 6: 
-                       hourDate.mes = "Junho";
-                       break;
-                    case 7: 
-                       hourDate.mes = "Julho";
-                       break;
-                    case 8: 
-                       hourDate.mes = "Agosto";
-                       break;
-                    case 9: 
-                       hourDate.mes = "Setembro";
-                       break;
-                    case 10: 
-                       hourDate.mes = "Outubro";
-                       break;
-                    case 11: 
-                       hourDate.mes = "Novembro";
-                       break;
-                    case 12: 
-                       hourDate.mes = "Dezembro";
-                       break;
-                    default:
-                       hourDate.mes = "Não se aplica.";
-                       break;
-                }
-
-            Console.WriteLine("\r{0} - {1} de {2} de {3} - {4}", 
-                hourDate.diaSemana, DateTime.Now.ToString("dd"),
-                hourDate.mes, DateTime.Now.ToString("yyyy"), DateTime.Now.ToString("HH:mm:ss"));
-
-                //método estático que sempre faz com que o thread atual entre no modo de suspensão.
-                Thread.Sleep(1000);    
-            }
+            hourDate.ApresentacaoAplicacao();
+            hourDate.DefinicaoDiaMesConsole();            
         }
     }
 }
